543b494ee2fb1bd846665694dd7c7304aa673c0c
afikanzoro <muchemimutahi@gmail.com>
2022-06-17 21:22:54 +0300

    Add await.js file

--- src/modules/dist/await.dev.js
+++ src/modules/dist/await.dev.js
@@ -1,1 +1,29 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _data = _interopRequireDefault(require("./data.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var Score = function Score() {
+  return regeneratorRuntime.async(function Score$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return regeneratorRuntime.awrap((0, _data["default"])());
+
+        case 2:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+};
+
+var _default = Score;
+exports["default"] = _default;
