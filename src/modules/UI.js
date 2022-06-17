088b29221e4b5e6c59130254534f30e1cd7a96c4
afikanzoro <muchemimutahi@gmail.com>
2022-06-17 21:16:00 +0300

    Make tweaks

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,13 +1,16 @@
 import { name, score } from './definition.js';
 
 const User = () => {
+    
   const userName = name.value;
   const userScore = score.value;
   const userInput = {
     user: userName,
+
     score: userScore,
   };
   return userInput;
+
 };
 
 export default User;
--- src/modules/await.js
+++
@@ -1,7 +0,0 @@
-import data from './data.js';
-
-const Score = async () => {
-  await data();
-};
-
-export default Score;
