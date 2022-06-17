5bbd81cb2ad0390d4817d9520b0e978832ededdb
afikanzoro <muchemimutahi@gmail.com>
2022-06-17 21:18:51 +0300

    Add data.js file

--- src/modules/dist/data.dev.js
+++ src/modules/dist/data.dev.js
@@ -1,1 +1,20 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var data = function data() {
+  scoreContainer.innerHTML = '';
+  fetch(gameURL).then(function (res) {
+    return res.json();
+  }).then(function (data) {
+    data.result.forEach(function (data) {
+      scoreContainer.innerHTML += "\n          <li>".concat(data.user, ": ").concat(data.score, "</li>");
+    });
+  });
+};
+
+var _default = data;
+exports["default"] = _default;
